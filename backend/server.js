c3cb9eb38f0b3404054afa333cd4f0aff325ad43
kwant88 <85560278+kwant88@users.noreply.github.com>
2022-07-07 15:33:12 +0200

    Ajout bcrypt/jsonwebtoken et models

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const userRoutes = require ('./routes/user');
 require('dotenv').config({path: './config/.env'})
 require('./config/db');
 const app = express();
+
+//Routes
+app.use('/api/user', userRoutes)
+//Server
 
 app.listen (process.env.PORT, () => {
   console.log(`Listening on port ${process.env.PORT}`)
