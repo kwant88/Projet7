090208ee5c22dfe8b34c2df1d6c0a667bb308955
kwant88 <85560278+kwant88@users.noreply.github.com>
2022-08-23 17:42:29 +0200

    Maj arborescence P7 + debug Home/App

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -15,9 +15,11 @@ const Home = () => {
 
     return(
         <div>
-            <h1>Accueil</h1>
             
+            <h1>Accueil</h1>
             
         </div>
     )
 }
+
+export default Home
